1f1d7fd9a497842f0f4174d569bc92da9304687c
Marc Muntané <mmclara1050@gmail.com>
2024-09-25 18:51:30 +0200

    EjArray 2 Correguido

--- EjerciciosArrays/EjArray2.php
+++ EjerciciosArrays/EjArray2.php
@@ -1,6 +1,5 @@
 <?php
 $nums2['nombre'] = 'Sara'; $nums2['apellido'] = 'Martinez'; $nums2['edad'] = 23; $nums2['ciudad'] = 'Barcelona';
-
 echo "<b>Ejercicio 2</b><br><br>";
 
 foreach( $nums2 as $clau => $valor){
