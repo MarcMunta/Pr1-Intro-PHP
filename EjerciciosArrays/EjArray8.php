92a2bfd4a2115e4480f22ed2e4e65236e1e8b3b2
Marc Muntané <mmclara1050@gmail.com>
2024-09-25 20:06:26 +0200

    EjArray8

---
+++ EjerciciosArrays/EjArray8.php
@@ -0,0 +1,21 @@
+<?php
+$array = [
+    "Marta" => 10,
+    "Isabel" => 8,
+    "Luís" => 7,
+    "Miguel" => 5,
+    "Aitor" => 4,
+    "Pepe" => 1
+];
+
+echo "<b>Ejercicio 8</b><br><br>";
+$mejor_alumno = "";
+$nota_mas_alta = 0;
+
+foreach ($array2 as $alumno => $nota) {
+    if ($nota > $nota_mas_alta) {
+        $nota_mas_alta = $nota;
+        $mejor_alumno = $alumno;
+    }
+}
+echo "La nota más alta es $nota_mas_alta y el mejor alumno es $mejor_alumno.";
